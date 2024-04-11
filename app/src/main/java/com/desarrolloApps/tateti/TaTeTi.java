9f3120f3d7d8e53e000b850f275f2465fe55537c
Ramiro Landajo <ramalandajo@gmail.com>
2024-04-11 11:29:25 -0300

    Logica modificada, interfaz basica

--- app/src/main/java/com/desarrolloApps/tateti/TaTeTi.java
+++ app/src/main/java/com/desarrolloApps/tateti/TaTeTi.java
@@ -35,28 +35,48 @@ public class TaTeTi {
     //Metodos de la clase
 
     //Este metodo va a llamar a los 3 metodos que verifican horizontal, vertical y diagonal
-    public boolean ganador(int fila,int columna,char valor){
+    public boolean chequearGanador(int fila,int columna,char valor){
 
         //Si alguno da true este metodo devuelve true. Si no false
 
-        if (verificarDiagonal(fila, columna) || verificarHorizontal(fila, columna) ||
+        if (verificarDiagonales(fila, columna) || verificarHorizontal(fila, columna) ||
                 verificarVertical(fila, columna)){
             return true;
         }else {
             return false;
         }
 
+
     }
 
     public boolean verificarVertical(int fila,int columna){
+        for (int i = 0; i < 3; i++) {
+            if (tablero[0][i] == tablero[1][i] && tablero[2][i] == tablero[0][i] && tablero[0][i] != ' ') {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean verificarHorizontal(int fila,int columna){
+        for (int i = 0; i < 3; i++) {
+            if (tablero[i][0] == tablero[i][1] && tablero[i][2] == tablero[0][i] && tablero[0][i] != ' ') {
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean verificarDiagonal(int fila,int columna){
+    public boolean verificarDiagonales(int fila,int columna){
+        //verificamos diagonal izq
+        if (tablero[0][0] == tablero[1][1] && tablero[2][2] == tablero[0][0] && tablero[0][0] != ' ') {
+            return true;
+        }
+
+        //verificamos diagonal derecha
+        if (tablero[0][2] == tablero[1][1] && tablero[2][0] == tablero[0][2] && tablero[0][2] != ' ') {
+            return true;
+        }
         return false;
     }
 }
